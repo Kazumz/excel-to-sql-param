0c9e5b6a3a592099470ccfe8381d7ef4fcdf47fe
Kallum Parr <kallum.parr@goodenergy.co.uk>
2022-02-10 15:25:23 +0000

    Initial commit

---
+++ KP.Excel.To.Sql.Param.Console/Models/Worksheet.cs
@@ -0,0 +1,7 @@
+﻿namespace KP.Excel.To.Sql.Param.Console.Models
+{
+    public class Worksheet
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ KP.Excel.To.Sql.Param.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using KP.Excel.To.Sql.Param.Console.Models;
+
+namespace KP.Excel.To.Sql.Param.Console
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var records = ReadWorksheetRecords();
+            System.Console.WriteLine($"Attempting to parse {records.Count()} records.");
+
+            foreach (var record in records)
+            {
+                System.Console.WriteLine($"\'{record.Email}\',");
+            }
+
+            System.Console.WriteLine("Press any key to close");
+            System.Console.ReadLine();
+        }
+
+        private static IEnumerable<Worksheet> ReadWorksheetRecords()
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                PrepareHeaderForMatch = args => args.Header.ToLower(),
+            };
+
+            using var reader = new StreamReader("..\\..\\..\\..\\worksheet.csv");
+            using var csv = new CsvReader(reader, config);
+            return csv.GetRecords<Worksheet>().ToList();
+        }
+    }
+}
